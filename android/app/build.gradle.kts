fef1fb7afacd7dd523c04a864c069f92f33a1677
pineyellow <237741264+pineyellow@users.noreply.github.com>
2026-07-05 19:27:39 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 import java.util.Properties
 
-val broguePeVersionName = "1.0.4"
+val broguePeVersionName = "1.0.5"
 
 plugins {
     id("com.android.application")
@@ -44,7 +44,7 @@ android {
         applicationId = "com.pineyellow.broguepe"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = broguePeVersionName
 
         externalNativeBuild {
